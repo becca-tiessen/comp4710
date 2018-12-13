e182b932e2755257501482bf4c60748ae3343adb
becca-tiessen <becca.tiessen@gmail.com>
2018-12-13 12:08:39 -0600

    Create wordCounter.py

---
+++ wordCounter.py
@@ -0,0 +1,36 @@
+import collections
+
+def removeUnwantedSymbols(wordlist):
+    wordlist = wordlist.replace(",","");
+    wordlist = wordlist.replace(".","");
+    wordlist = wordlist.replace("!","");
+    wordlist = wordlist.replace("@","");
+    wordlist = wordlist.replace("#","");
+    wordlist = wordlist.replace("$","");
+    wordlist = wordlist.replace("%","");
+    wordlist = wordlist.replace("^","");
+    wordlist = wordlist.replace("&","");
+    wordlist = wordlist.replace("*","");
+    wordlist = wordlist.replace("()","");
+    wordlist = wordlist.replace(")","");
+    wordlist = wordlist.replace("-","");
+    wordlist = wordlist.replace("}","");
+    wordlist = wordlist.replace("{","");
+    wordlist = wordlist.replace("=","");
+    wordlist = wordlist.replace("|","");
+    return wordlist
+
+file = open('FemaleQuestion.txt', 'r')
+wordstring = file.read()
+
+wordlist = removeUnwantedSymbols(wordstring)
+wordlist = wordlist.lower().split()
+
+trimmedlist = [];
+
+for word in wordlist:
+    if "<" not in word or ">" not in word:
+        trimmedlist.append(word)
+
+freq = collections.Counter(trimmedlist)
+print freq.most_common(10)
